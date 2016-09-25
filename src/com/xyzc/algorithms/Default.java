a0541fa69dc2e50a1b427e1f3fb1c751f5ed6518
xyzc <codercheng@qq.com>
2016-09-25 09:41:31 +0800

    add local
    Signed-off-by: xyzc <codercheng@qq.com>

---
+++ src/com/xyzc/algorithms/Default.java
@@ -0,0 +1,10 @@
+package com.xyzc.algorithms;
+
+/**
+ * Created by xyzc on 2016/9/24.
+ */
+public class Default {
+    public void test(){
+        System.out.println("testman");
+    }
+}
---
+++ test/com/xyzc/algorithms/DefaultTest.java
@@ -0,0 +1,15 @@
+package com.xyzc.algorithms;
+
+import org.junit.Test;
+
+/**
+ * Created by xyzc on 2016/9/24.
+ */
+
+public class DefaultTest {
+    @Test
+    public void test(){
+        Default aDefault = new Default();
+        aDefault.test();
+    }
+}
